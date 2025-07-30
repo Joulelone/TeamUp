3023bd2b2fdfb94604a3e6808ed0283ac9acc221
Jalel Belkacem <jalel.belkacem78@gmail.com>
2025-07-30 13:41:19 +0200

    [ADD] Premier commit avec juste la base de react mais pas le reste disponible sur une version test

---
+++ teamup/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>👥 TeamUp</h1>
+      <p>Bienvenue dans ton projet collaboratif React !</p>
+    </div>
+  );
+}
+
+export default App;
